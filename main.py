003fe72111c8bfca08b2b63b8e5f19fd57517ba8
yang761127 <1536520643@qq.com>
2022-03-02 14:10:49 +0800

    test

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ import pytest
 if __name__ == '__main__':
     pytest.main()
     time.sleep(3)
-    os.system("allure generate ./reports -o ./results --clean")
+    os.system("allure generate ./results -o ./reports --clean")
